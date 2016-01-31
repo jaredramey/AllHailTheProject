4baee9e83a25b168e1f23f342efe0acf6c8544c5
JeffreyMJohnson <jefjohms@gmail.com>
2016-01-30 22:14:18 -0800

    working on line graph for shiggles

---
+++ AllHailTheButton/Assets/Scripts/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class User
+    {
+        private static int nextID = 1;
+
+        public int ID
+        {
+            get
+            {
+                return mID;
+            }
+
+        }
+        private int mID;
+        
+
+        public User()
+        {
+            mID = nextID++;
+        }
+    }
+}
