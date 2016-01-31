49db92827cecc1d0236dfb0d6816bb7535e7f987
JeffreyMJohnson <jefjohms@gmail.com>
2016-01-30 18:25:53 -0800

    added files from prev commit (time manager) and hopefully fixed the gitignore. the library files were being picked up.

---
+++ AllHailTheButton/Assets/Scripts/TimeManTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class TimeManTest : MonoBehaviour
+{
+    Text text;
+    TimeManager timeMan;
+    int eventCount = 0;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+
+    }
+
+    void Start()
+    {
+        timeMan = FindObjectOfType<TimeManager>();
+        timeMan.TimeIsUp += Trigger;
+    }
+
+    private void Trigger(object sender, EventArgs e)
+    {
+        text.text = "Trigger events received: " + ++eventCount;
+    }
+}
